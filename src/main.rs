d80a3dca975835e537fa30610bf1e732d0c2ab70
Ashley Wulber <ashley@system76.com>
2024-01-16 14:41:44 -0500

    chore: add optional arguments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,69 @@
 use ashpd::desktop::screenshot::Screenshot;
-use std::fs;
+use clap::{command, ArgAction, Parser};
+use std::{collections::HashMap, fs, os::unix::fs::MetadataExt, path::PathBuf};
+use zbus::{dbus_proxy, zvariant::Value, Connection};
+
+#[derive(Parser, Default, Debug, Clone, PartialEq, Eq)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Enable interactive mode in the portal
+    #[clap(long,
+        default_missing_value("true"),
+        default_value("true"),
+        num_args(0..=1),
+        require_equals(true),
+        action = ArgAction::Set)]
+    interactive: bool,
+    /// Enable modal mode in the portal
+    #[clap(long,
+        default_missing_value("true"),
+        default_value("true"),
+        num_args(0..=1),
+        require_equals(true),
+        action = ArgAction::Set,)]
+    modal: bool,
+    /// Send a notification with the path to the saved screenshot
+    #[clap(long,
+        default_missing_value("true"),
+        default_value("true"),
+        num_args(0..=1),
+        require_equals(true),
+        action = ArgAction::Set)]
+    notify: bool,
+    /// The directory to save the screenshot to, if not performing an interactive screenshot
+    #[clap(short, long)]
+    save_dir: Option<PathBuf>,
+}
+
+#[dbus_proxy(assume_defaults = true)]
+trait Notifications {
+    /// Call the org.freedesktop.Notifications.Notify D-Bus method
+    fn notify(
+        &self,
+        app_name: &str,
+        replaces_id: u32,
+        app_icon: &str,
+        summary: &str,
+        body: &str,
+        actions: &[&str],
+        hints: HashMap<&str, &Value<'_>>,
+        expire_timeout: i32,
+    ) -> zbus::Result<u32>;
+}
 
 //TODO: better error handling
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let picture_dir = dirs::picture_dir().expect("failed to locate picture directory");
-
-    let date = chrono::Local::now();
-    let filename = format!("Screenshot_{}.png", date.format("%Y-%m-%d_%H-%M-%S"));
-    let path = picture_dir.join(filename);
+    let args = Args::parse();
+    let picture_dir = (!args.interactive).then(|| {
+        args.save_dir
+            .filter(|dir| dir.is_dir())
+            .unwrap_or_else(|| dirs::picture_dir().expect("failed to locate picture directory"))
+    });
 
     let response = Screenshot::request()
-        .interactive(true)
-        .modal(true)
+        .interactive(args.interactive)
+        .modal(args.modal)
         .send()
         .await
         .expect("failed to send screenshot request")
@@ -20,12 +71,57 @@ async fn main() {
         .expect("failed to receive screenshot response");
 
     let uri = response.uri();
-    let tmp_path = match uri.scheme() {
-        "file" => uri.path(),
+    let path = match uri.scheme() {
+        "file" => {
+            if let Some(picture_dir) = picture_dir {
+                let date = chrono::Local::now();
+                let filename = format!("Screenshot_{}.png", date.format("%Y-%m-%d_%H-%M-%S"));
+                let path = picture_dir.join(filename);
+                let tmp_path = uri.path();
+                if fs::metadata(&picture_dir)
+                    .expect("Failed to get medatata on filesystem for screenshot destination")
+                    .dev()
+                    != fs::metadata(tmp_path)
+                        .expect("Failed to get metadata on filesystem for temporary path")
+                        .dev()
+                {
+                    // copy file instead
+                    fs::copy(tmp_path, &path).expect("failed to move screenshot");
+                    fs::remove_file(tmp_path).expect("failed to remove temporary screenshot");
+                } else {
+                    fs::rename(tmp_path, &path).expect("failed to move screenshot");
+                }
+
+                path.to_string_lossy().to_string()
+            } else {
+                uri.path().to_string()
+            }
+        }
         scheme => panic!("unsupported scheme '{}'", scheme),
     };
 
-    fs::copy(tmp_path, &path).expect("failed to copy screenshot file");
+    println!("{path}");
 
-    println!("{}", path.display());
+    if args.notify {
+        let connection = Connection::session()
+            .await
+            .expect("failed to connect to session bus");
+
+        let proxy = NotificationsProxy::new(&connection)
+            .await
+            .expect("failed to create proxy");
+        _ = proxy
+            .notify(
+                "Cosmic Screenshot",
+                0,
+                "camera-photo-symbolic",
+                &path,
+                "",
+                &[],
+                HashMap::new(),
+                5000,
+            )
+            .await
+            .expect("failed to send notification");
+    }
 }
